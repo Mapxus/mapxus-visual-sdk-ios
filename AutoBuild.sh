101427303890b168c367fdad0a421d12bbdb8b83
chenghaoguo <chenghaoguo@mapxus.com>
2021-08-24 10:39:04 +0800

    Change to use cocoapods with bundle.

--- AutoBuild.sh
+++ AutoBuild.sh
@@ -104,6 +104,6 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-pod install
+bundle exec pod install --repo-update
 xcodebuild -workspace MapxusVisualSDK.xcworkspace -scheme MapxusVisualSDK-Universal POD_DIR="$DYNAMIC_DIR" XCCONFIG_FILE="$XCCONFIG_FILE"
 

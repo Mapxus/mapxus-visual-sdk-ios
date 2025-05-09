a606995c1276400133c0ffa019f1e798f2ce1fbc
chenjiajie <jiajiechen@mapxus.com>
2025-05-09 17:08:20 +0800

    # CHANGELOG
    ## v7.5.0 (2025-05-09)
    
    ### 📝 Changes
    
    * Synchronous version.

--- MapxusVisualSDK.podspec
+++ MapxusVisualSDK.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |s|
 
 
-  version = '7.4.0'
+  version = '7.5.0'
 
   s.name         = 'MapxusVisualSDK'
   s.version      = version
@@ -13,7 +13,7 @@ Pod::Spec.new do |s|
   s.license      = { :type => 'BSD 3-Clause', :file => 'LICENSE' }
   s.author       = { 'Mapxus' => 'developer@maphive.io' }
 
-  s.platform     = :ios, '9.0'
+  s.platform     = :ios, '13.0'
 
   s.source       = { :http => "https://nexus3.mapxus.com/repository/ios-sdk/#{version.to_s}/mapxus-visual-sdk-ios.zip", :flatten => true }
 
--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@
 
 import PackageDescription
 
-let version = "7.4.0"
+let version = "7.5.0"
 
 let package = Package(
   name: "MapxusVisualSDK",
   platforms: [
-    .iOS(.v9),
+    .iOS(.v13),
   ],
   products: [
     .library(
@@ -19,7 +19,7 @@ let package = Package(
     .binaryTarget(
       name: "MapxusVisualSDK",
       url: "https://nexus3.mapxus.com/repository/ios-sdk/\(version)/mapxus-visual-sdk-ios.zip",
-      checksum: "67e40221a66680ce60d85c5b04e6c1d197103d4e22fe1c49f7e0ae65e5093535"
+      checksum: "f45029a0b72d8833b271f993b8ae34a91666226b061bfc725cb523e080eb628d"
     )
   ]
 )
